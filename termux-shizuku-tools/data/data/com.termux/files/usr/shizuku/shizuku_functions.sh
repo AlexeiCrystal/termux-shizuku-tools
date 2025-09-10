f65b2ffa1c14bf568404314a7cb94544df4f697d
AlexeiCrystal <alex211952i@gmail.com>
2025-09-10 20:58:53 +0300

    Update shizuku_functions.sh

--- termux-shizuku-tools/data/data/com.termux/files/usr/shizuku/shizuku_functions.sh
+++ termux-shizuku-tools/data/data/com.termux/files/usr/shizuku/shizuku_functions.sh
@@ -2,19 +2,24 @@
 
 is_shizuku_installed() {
     if cmd package list packages | grep -q "package:moe.shizuku.privileged.api"; then
-        return 0
+        echo true
     else
-        return 1
+        echo false
     fi
 }
 
 shizuku_version_code() {
-    if is_shizuku_installed; then
+    if [ "$(is_shizuku_installed)" = "true" ]; then
         cmd package list packages --show-versioncode | grep "package:moe.shizuku.privileged.api versionCode:" | awk -F':' '{print $NF}'
     else
         echo "-1"
     fi
 }
 
+execute_shizuku_command() {
+    $PREFIX/shizuku/rish -c "$1"
+}
+
 export is_shizuku_installed
 export shizuku_version_code
+export execute_shizuku_command
