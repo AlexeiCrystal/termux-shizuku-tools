c5fe525e872a7e39571e028b53a5f27e21a640eb
AlexeiCrystal <alex211952i@gmail.com>
2025-09-11 08:26:37 +0300

    Update shizuku_functions.sh

--- termux-shizuku-tools/data/data/com.termux/files/usr/shizuku/shizuku_functions.sh
+++ termux-shizuku-tools/data/data/com.termux/files/usr/shizuku/shizuku_functions.sh
@@ -1,5 +1,13 @@
 #!/data/data/com.termux/files/usr/bin/sh
 
+termux_shizuku_tools_version() {
+    echo "3.0"
+}
+
+termux_shizuku_tools_directory() {
+    echo "$PREFIX/shizuku"
+}
+
 is_shizuku_installed() {
     if cmd package list packages | grep -q "package:moe.shizuku.privileged.api"; then
         echo true
@@ -20,6 +28,8 @@ execute_shizuku_command() {
     $PREFIX/shizuku/rish -c "$1"
 }
 
+export termux_shizuku_tools_version
+export termux_shizuku_tools_directory
 export is_shizuku_installed
 export shizuku_version_code
 export execute_shizuku_command
