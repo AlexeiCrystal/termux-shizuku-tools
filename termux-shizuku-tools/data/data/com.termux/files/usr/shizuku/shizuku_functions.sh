4d4cf28bb305cedd86f07d91d4967f63502ab8fc
AlexeiCrystal <alex211952i@gmail.com>
2025-09-12 09:01:32 +0300

    Update shizuku_functions.sh

--- termux-shizuku-tools/data/data/com.termux/files/usr/shizuku/shizuku_functions.sh
+++ termux-shizuku-tools/data/data/com.termux/files/usr/shizuku/shizuku_functions.sh
@@ -3,14 +3,17 @@
 termux_shizuku_tools_version() {
     dpkg -s termux-shizuku-tools | grep '^Version:' | awk '{print $2}'
 }
+export termux_shizuku_tools_version
 
 termux_shizuku_tools_version_code() {
     dpkg -s termux-shizuku-tools | grep '^X-Version-Code:' | awk '{print $2}'
 }
+export termux_shizuku_tools_version_code
 
 termux_shizuku_tools_directory() {
     echo "$PREFIX/shizuku"
 }
+export termux_shizuku_tools_directory
 
 is_shizuku_installed() {
     if cmd package list packages | grep -q "package:moe.shizuku.privileged.api"; then
@@ -19,6 +22,7 @@ is_shizuku_installed() {
         echo false
     fi
 }
+export is_shizuku_installed
 
 shizuku_version_code() {
     if [ "$(is_shizuku_installed)" = "true" ]; then
@@ -27,24 +31,30 @@ shizuku_version_code() {
         echo "-1"
     fi
 }
+export shizuku_version_code
 
 execute_shizuku_command() {
     $PREFIX/shizuku/rish -c "$1"
 }
+export execute_shizuku_command
 
 open_shizuku() {
     am start -n moe.shizuku.privileged.api/moe.shizuku.manager.MainActivity
 }
+export open_shizuku
 
 stop_shizuku_server() {
     execute_shizuku_command "kill \$(pidof shizuku_server)"
 }
-
 export stop_shizuku_server
-export termux_shizuku_tools_version
-export termux_shizuku_tools_directory
-export is_shizuku_installed
-export shizuku_version_code
-export execute_shizuku_command
-export open_shizuku
-export termux_shizuku_tools_version_code
+
+is_shizuku_server_running() {
+    CMD_OUTPUT=$(execute_shizuku_command "echo shizuku_server_check")
+    
+    if [ "$CMD_OUTPUT" = "shizuku_server_check" ]; then
+        echo "true"
+    else
+        echo "false"
+    fi
+}
+export is_shizuku_server_running
