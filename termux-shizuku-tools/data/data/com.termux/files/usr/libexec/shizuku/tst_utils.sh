fe49334e0988bfa87ed08efd32f68220d0052a09
AlexeiCrystal <alex211952i@gmail.com>
2026-02-07 13:40:26 +0300

    Rewrite some code

--- termux-shizuku-tools/data/data/com.termux/files/usr/libexec/shizuku/tst_utils.sh
+++ termux-shizuku-tools/data/data/com.termux/files/usr/libexec/shizuku/tst_utils.sh
@@ -1,16 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-TST_PKG="termux-shizuku-tools"
-TST_GH_REPO_LINK="https://github.com/AlexeiCrystal/termux-shizuku-tools"
-
-SHK_NOT_INST_ERR_MSG="Error: Shizuku is not installed"
-SHK_UNK_CMD_ERR_MSG="Unknown command:"
-SHK_LAUNCH_MSG="Launching Shizuku app..."
-SHK_STOP_MSG="Stopping Shizuku server..."
-SHK_I_SH_MSG="Starting Shizuku interactive shell..."
-SHK_I_SH_EXIT_TIP="Type 'exit' to exit"
-HELP_CMD_TIP="Use 'shizuku help' for available commands"
-
 G='\033[0;32m'
 R='\033[0;31m'
 Y='\033[0;33m'
@@ -19,6 +8,30 @@ NC='\033[0m'
 
 TICK="${G}✔${NC}"
 CROSS="${R}✖${NC}"
+
+TST_PKG="termux-shizuku-tools"
+TST_GH_REPO_LINK="https://github.com/AlexeiCrystal/termux-shizuku-tools"
+
+SHK_WEB_URL="https://shizuku.rikka.app"
+
+DOTS="..."
+
+SHK_NOT_INST_ERR_MSG="Error: Shizuku is not installed"
+SHK_UNK_CMD_ERR_MSG="Unknown command:"
+HELP_CMD_TIP="Use 'shizuku help' for available commands"
+
+SHK_LAUNCH_MSG="Launching Shizuku app$DOTS"
+SHK_STOP_MSG="Stopping Shizuku server$DOTS"
+
+SHK_I_SH_MSG="Starting Shizuku interactive shell$DOTS"
+SHK_I_SH_EXIT_TIP="Type 'exit' to exit"
+
+SHK_SERV_STAT_STR_RUN=" \033[1A\033[KServer:    ${TICK} Running"
+SHK_SERV_STAT_STR_STOP=" \033[1A\033[KServer:    ${CROSS} Stopped"
+SHK_SERV_STAT_STR_LOADING=" Server:    Loading info${DOTS}"
+
+SHK_INST_STAT_STR_YES=" ${NC}Installed: ${TICK} Yes"
+SHK_INST_STAT_STR_NO=" ${NC}Installed: ${CROSS} No"
 
 export TST_PKG \
        TST_GH_REPO_LINK \
@@ -74,24 +87,24 @@ EOF
 show_status() {
     source "$PREFIX/libexec/shizuku/shizuku_lib.sh"
     echo -e "${NC} Package:   ${Y}${SHK_PKG}${NC}"
-    echo -e "${NC} Website:   ${B}https://shizuku.rikka.app${NC}"
+    echo -e "${NC} Website:   ${B}$SHK_WEB_URL${NC}"
     
     local installed=$(is_shk_installed)
     
     if [ "$installed" = "true" ]; then
-        echo -e "${NC} Installed: ${TICK} Yes"
+        echo -e "$SHK_INST_STAT_STR_YES"
         
         local v_code=$(get_shk_version_code)
         echo -e "${NC} Version:   ${Y}${v_code}${NC}"
-        echo -e " Server:    Loading info..."
+        echo -e "$SHK_SERV_STAT_STR_LOADING"
         local running=$(is_shk_server_running)
         if [ "$running" = "true" ]; then
-            echo -e "\033[1A\033[K${NC} Server:    ${TICK} Running"
+            echo -e "$SHK_SERV_STAT_STR_RUN"
         else
-            echo -e "\033[1A\033[K${NC} Server:    ${CROSS} Stopped"
+            echo -e "$SHK_SERV_STAT_STR_STOP"
         fi
     else
-        echo -e "${NC} Installed: ${CROSS} No"
+        echo -e "$SHK_INST_STAT_STR_NO"
     fi
 
 }
